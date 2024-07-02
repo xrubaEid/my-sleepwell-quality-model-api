ac85ecd7ddc3784af1af540af115d755e18f404f
xrubaEid <143491851+xrubaEid@users.noreply.github.com>
2024-07-03 02:42:45 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-import pickle
+import joblib
 import json
 import pandas as pd
 
@@ -17,7 +17,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-class ModelInput(BaseModel):
+class SleepQualityInput(BaseModel):
     Q1: str
     Q2: str
     Q3: str
@@ -26,32 +26,49 @@ class ModelInput(BaseModel):
     Q6: str
     Q7: str
 
-# تحميل النموذج المحفوظ
-model = pickle.load(open('sleepwell_quality_trained_model.sav', 'rb'))
-label_encoder = pickle.load(open('label_encoder.pkl', 'rb'))
+# تحميل النموذج المدرب و LabelEncoder
+best_model = joblib.load('best_model.pkl')
+label_encoder = joblib.load('label_encoder.pkl')
 
 @app.post('/predict_sleep_quality')
-def predict_sleep_quality(input_data: ModelInput):
-    input_dict = input_data.dict()
-    input_df = pd.DataFrame([input_dict])
-    
+def predict_sleep_quality(input_parameters: SleepQualityInput):
+    input_data = input_parameters.json()
+    input_dictionary = json.loads(input_data)
+
+    answers = [
+        input_dictionary['Q1'],
+        input_dictionary['Q2'],
+        input_dictionary['Q3'],
+        input_dictionary['Q4'],
+        input_dictionary['Q5'],
+        input_dictionary['Q6'],
+        input_dictionary['Q7']
+    ]
+
     # تحويل المدخلات إلى نفس التنسيق المستخدم في التدريب
+    input_data = {
+        'Q1': [answers[0]],
+        'Q2': [answers[1]],
+        'Q3': [answers[2]],
+        'Q4': [answers[3]],
+        'Q5': [answers[4]],
+        'Q6': [answers[5]],
+        'Q7': [answers[6]]
+    }
+
+    input_df = pd.DataFrame(input_data)
     input_df = pd.get_dummies(input_df)
-    
+
     # ضمان أن تكون الأعمدة في المدخلات الجديدة تتطابق مع الأعمدة المستخدمة في التدريب
-    missing_cols = set(model.feature_names_in_) - set(input_df.columns)
+    missing_cols = set(best_model.feature_names_in_) - set(input_df.columns)
     for col in missing_cols:
         input_df[col] = 0
-    input_df = input_df[model.feature_names_in_]
-    
+    input_df = input_df[best_model.feature_names_in_]
+
     # تنفيذ التنبؤ
-    predicted_quality_encoded = model.predict(input_df)
-    
+    predicted_quality_encoded = best_model.predict(input_df)
+
     # تحويل التنبؤ إلى التصنيف الأصلي
     predicted_quality = label_encoder.inverse_transform(predicted_quality_encoded)
-    
-    return {"Predicted Sleep Quality": predicted_quality[0]}
 
-if __name__ == '__main__':
-    import uvicorn
-    uvicorn.run(app, host='0.0.0.0', port=8000)
+    return {'Predicted Sleep Quality': predicted_quality[0]}
